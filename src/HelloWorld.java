4b1f682bde8010319d3c1f4cfaaa790de76a2413
simachong <simachong@192.168.0.119>
2018-01-14 23:16:53 +0800

    t3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("GitHubTest");
 		System.out.println("GitHubFromMacbook!");
 		System.out.println("GitHubFromThinkpad!");
+		System.out.println("Big bang!");
 
 	}
 
