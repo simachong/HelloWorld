0ec5b2e282d9727e27f4995ee1a82c3e345fbbd5
Administrator <Administrator@UWN9HWB4FMWAUHF>
2018-01-14 22:51:50 +0800

    thinkpad changed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld!");
 		System.out.println("GitHubTest");
+		System.out.println("GitHubFromMacbook!");
+		System.out.println("GitHubFromThinkpad!");
 	}
 
 }
